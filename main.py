00af716de005f018ea209e303b1be88ad5e7c732
wode808 <wode@leo.pp.ua>
2026-05-05 16:30:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except ImportError:
 # 配置区域
 # ==============================================================================
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=6666666-6666-6666-6666-66666666666",
+    "https://host2play.gratis/server/renew?i=ba3a6c99-c58f-4ccb-a9fd-e80e26385311",
     # 添加更多链接
 ]
 
